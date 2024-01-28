9ff75caefac7bc738bc3a9f213ede926227507be
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2024-01-28 15:45:13 +0100

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -22,7 +22,7 @@ sudo apt install curl -y
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 
 # ftp
-sudo apt install vsftp -y
+sudo apt install vsftpd -y
 
 # power management
 sudo apt install tlp -y
