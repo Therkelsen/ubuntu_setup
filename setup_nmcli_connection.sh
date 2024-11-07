6a672452dccebf6d44cf1c1331e45a1dec25e562
Therkelsen <thomastherkelsen191200@gmail.com>
2024-11-07 09:32:20 +0100

    Improved nmcli

--- setup_nmcli_connection.sh
+++ setup_nmcli_connection.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# add help command (wrong amount of args, or -h or --help)
 if [ $# -ne 2 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
     echo "Usage: ./connect.sh <connection_name> <interface_name>"
     echo "Interface name found by running ifconfig -a. Something containing enx."
@@ -9,17 +10,25 @@ fi
 sudo nmcli connection add con-name $1 ifname $2 type ethernet
 sudo nmcli dev set $2 managed yes
 
-sudo mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf_orig 
+sudo mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf /usr/lib/NetworkManager/conf.d/1>
 sudo touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
 
 sudo systemctl restart NetworkManager
 
 sudo nmcli con up $1
 
+echo " "
+sudo nmcli connection show
+
+echo " "
 ping -c 1 google.com
-if [$? -ne 0]; then
-    echo "Connection failed"
+if [ $? -ne 0 ]; then
+    echo -e "\nConnection failed."
 fi
 
-echo "Connection successfully established"
-ifconfig -a | grep -E inet
+echo -e "\nConnection successfully established."
+echo -e "\nGrab your IP from here."
+ifconfig -a | grep -E "inet" | grep -v "inet6" | grep -v "127.0.0.1"
+
+echo -e "\nScript done"
+
