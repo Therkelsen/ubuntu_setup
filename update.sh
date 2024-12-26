340b0dda2b8eee81f7654d613ce82f3a8953f8ce
Therkelsen <thomastherkelsen191200@gmail.com>
2024-12-26 18:28:20 +0100

    Using apt-fast for updates

--- update.sh
+++ update.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean
-sudo deborphan | xargs sudo apt -y remove --purge
+# Update and upgrade the system
+sudo apt-fast update
+sudo apt-fast full-upgrade -y
+sudo apt-fast autoremove -y
+sudo apt-fast autoclean
+sudo deborphan | xargs sudo apt-fast -y remove --purge
+
+# Refresh snaps
 sudo snap refresh
-sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+
+# Remove unused flatpaks
+flatpak uninstall --unused -y
+
+# Check if the -ven parameter is passed
+if [[ "$1" == "-ven" ]]; then
+    echo "Installing Vencord..."
+    sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+else
+    echo "Skipping Vencord installation. Pass -ven to install it."
+fi
