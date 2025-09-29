c692b800a06cea3b7e10489ccf99c155175eeabf
thth <thth@mir-robots.com>
2025-09-29 14:28:02 +0200

    Update standard setup

--- standard_setup.sh
+++ standard_setup.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
+# Add multiverse repository
+sudo add-apt-repository multiverse
+
+# Add Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
 # apt fast
 sudo add-apt-repository ppa:apt-fast/stable
-sudo apt-get update
-sudo apt-get -y install apt-fast
+sudo apt update
+sudo apt -y install apt-fast
 
 # prerequisites
-sudo apt install -y gpg python3-dev python3-pip python3-setuptools curl
+sudo apt install -y apt-transport-https ca-certificates containerd.io curl curl docker-ce docker-ce-cli duf flatpak git gnome-software-plugin-flatpak gnupg-agent gpg python3 python3-dev python3-pip python3-setuptools software-properties-common
+
+# flatpak things
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.discordapp.Discord -y
+# Vencord
+sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+
+# Brave
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# VS Code Insiders
+sudo snap install code-insiders --classic -y
 
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.bash & wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.bash & wget https://github.com/junegunn/fzf/releases/download/v0.65.2/fzf-0.65.2-linux_amd64.tar.gz
+tar -xzf fzf-0.65.2-linux_amd64.tar.gz & sudo mv fzf /usr/local/bin/
+source ~/key-bindings.bash 
+source ~/completion.bash
 
 # The Fuck
-pip3 install thefuck --user
-echo 'eval $(thefuck --alias)' >> ~/.bashrc
+# pip3 install thefuck --user
+# echo 'eval $(thefuck --alias)' >> ~/.bashrc
 
 # eza
 sudo mkdir -p /etc/apt/keyrings
@@ -30,8 +52,28 @@ mkdir -p ~/.config/eza
 ln -sf "$(pwd)/eza-themes/themes/one_dark.yml" ~/.config/eza/theme.ym
 
 # duf
-sudo apt install -y duf
 echo 'alias df="duf --sort size --hide special"' >> ~/.bashrc
+
+# Gnome stuff
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+
+# Docker stuff
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
+
+# Git setup
+git config --global credential.helper store
+git config --global user.name "Therkelsen"
+git config --global user.email "thomastherkelsen191200@gmail.com"
+ssh-keygen -t ed25519
+eval 'ssh-agent'
+ssh-add ~/.ssh/id_ed25519
+echo -e 'Here is your SSH public key (Copy it and paste into GitHub under SSH and GPG keys):\n'
+cat ~/.ssh/id_ed25519.pub
+
+# Python stuff
+sudo apt-get install -y python3-pip
 
 # Git aliases to speed up workflow
 echo 'alias gfr="git fetch && git rebase origin/develop --autostash"' >> ~/.bashrc
